92e34d084e3115308fd4cd0a9832866285ee55f6
Karl <smitzkar@gmail.com>
2024-03-28 15:05:50 +0100

    Add Navbar.vue

--- audioguide/src/App.vue
+++ audioguide/src/App.vue
@@ -2,28 +2,20 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import Controls from './components/Controls.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Welcome" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/language">Language</RouterLink>
-        <a href="https://radbahn.berlin">Radbahn Berlin</a>
-      </nav>
-    </div>
+    <Navbar/>
   </header>
 
-  <!-- import components here -->
-  <RouterView />
-  <Controls />
-  
+  <body>
+    <HelloWorld msg="Hello Test"/>
+    <Controls/>
+  </body>
+
 </template>
 
 <style scoped>
--- audioguide/src/components/Navbar.vue
+++ audioguide/src/components/Navbar.vue
@@ -1,17 +1,24 @@
-<script>
-
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+
+<!-- Do I put <header> in this component? Or an empty header in App.vue and fill it with the navbar? -->
 <template>
-<nav class="menu">
-    <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-    </ul>
-</nav>
+
+  <div class="wrapper">
+    <nav>
+      <a href="https://radbahn.berlin"><img src="@/assets/TEMPORARY-radbahnSchriftzug.png" alt="Radbahn" class="logo" style="width:auto;height:42px;"></a>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/language">Language</RouterLink>
+    </nav>
+  </div>
+
+  <!-- import components here -->
+  <RouterView />
+  
 </template>
 
-<style>
-
+<style scoped>
 </style>
