039bb6ebe7b53d549cf81044a2471c42201e8919
Karl <smitzkar@gmail.com>
2024-03-28 14:10:30 +0100

    Add new Navbar.vue to simplify things for how I like it

---
+++ audioguide/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+<nav class="menu">
+    <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+</nav>
+</template>
+
+<style>
+
+</style>
